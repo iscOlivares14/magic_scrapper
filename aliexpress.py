1360e13ee2708d6038e6e22973fa29e2b0834493
Victor Emmanuel Olivares Rosas <isc.victorolivares@gmail.com>
2024-09-28 00:00:50 -0600

    abstract behavior in a baseClass

--- aliexpress.py
+++ aliexpress.py
@@ -1,62 +1,51 @@
-import random
-import time
+
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver import ActionChains
+
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
+
+from base_scrapper import BaseSelemiunScrapper
 
 from utils import download_image
 
 # item_url = 'https://es.aliexpress.com/item/1005006376464306.html?spm=a2g0o.order_list.order_list_main.4.347f194dP7bqst&gatewayAdapt=glo2esp'
 item_url = 'https://es.aliexpress.com/item/1005006895003798.html?spm=a2g0o.order_list.order_list_main.40.347f194dlOF89p&gatewayAdapt=glo2esp'
+# item_url = "https://es.aliexpress.com/item/1005007221056183.html?spm=a2g0o.detail.pcDetailBottomMoreOtherSeller.3.4113v1VRv1VRXP&gps-id=pcDetailBottomMoreOtherSeller&scm=1007.40050.354490.0&scm_id=1007.40050.354490.0&scm-url=1007.40050.354490.0&pvid=e686259c-ca2c-4369-a265-5a60ef4cbcb8&_t=gps-id:pcDetailBottomMoreOtherSeller,scm-url:1007.40050.354490.0,pvid:e686259c-ca2c-4369-a265-5a60ef4cbcb8,tpp_buckets:668%232846%238116%232002&pdp_npi=4%40dis%21MXN%21536.28%21413.02%21%21%2126.80%2120.64%21%40210318c317273708776387970e68b7%2112000039859310573%21rec%21MX%213220716497%21X&utparam-url=scene%3ApcDetailBottomMoreOtherSeller%7Cquery_from%3A"
 
-SCROLL_DELAY_TIMES = [1,2,3,4,5]
 OUTPUT_DIR = "images/output"
 
-options = webdriver.ChromeOptions()
-options.add_argument("--start-maximized")
+class AliScrapper(BaseSelemiunScrapper):
 
-driver = webdriver.Chrome(options=options)
-driver.implicitly_wait(3)
-driver.get(item_url)
+    def scrape(self):
+        self.init_driver(implicitly_wait=3)
+        print("initialized")
+        self._driver.get(item_url)
 
-def scroll_and_wait(driver, dom_element, delta_x=0, delta_y=0, delay_seconds=None):
-    scroll_origin = ScrollOrigin.from_element(dom_element)
-    ActionChains(driver).scroll_from_origin(scroll_origin, delta_x, delta_y).perform()
-    delay_seconds = delay_seconds if delay_seconds else random.choice(SCROLL_DELAY_TIMES)
-    time.sleep(delay_seconds)    
+        # take advantage of the implicit wait to pass the page fully_loaded to make the soup
+        div_product_summary = self._driver.find_element(By.CSS_SELECTOR, "div.pdp-info")
+        self.scroll_and_wait(div_product_summary, delta_y=600)
+        div_product_review = self._driver.find_element(By.CSS_SELECTOR, "div#nav-review")
+        self.scroll_and_wait(div_product_review, delta_y=600)
+        div_product_specs = self._driver.find_element(By.CSS_SELECTOR, "div#nav-specification")
+        self.scroll_and_wait(div_product_specs, delta_y=600)
 
-# take advantage of the implicit wait to pass the page fully_loaded to make the soup
-div_product_summary = driver.find_element(By.CSS_SELECTOR, "div.pdp-info")
-scroll_and_wait(driver, div_product_summary, delta_y=600)
-# scroll_origin = ScrollOrigin.from_element(div_product_summary)
-# ActionChains(driver).scroll_from_origin(scroll_origin, 0, 600).perform()
-# time.sleep(random.choice(SCROLL_DELAY_TIMES))
-div_product_review = driver.find_element(By.CSS_SELECTOR, "div#nav-review")
-scroll_and_wait(driver, div_product_review, delta_y=600)
-# scroll_origin = ScrollOrigin.from_element(div_product_review)
-# ActionChains(driver).scroll_from_origin(scroll_origin, 0, 600).perform()
-# time.sleep(random.choice(SCROLL_DELAY_TIMES))
-div_product_specs = driver.find_element(By.CSS_SELECTOR, "div#nav-specification")
-scroll_and_wait(driver, div_product_specs, delta_y=600)
-# scroll_origin = ScrollOrigin.from_element(div_product_specs)
-# ActionChains(driver).scroll_from_origin(scroll_origin, 0, 600).perform()
-# time.sleep(random.choice(SCROLL_DELAY_TIMES))
+        soup = BeautifulSoup(self._driver.page_source, 'lxml')
 
-soup = BeautifulSoup(driver.page_source, 'lxml')
+        self._driver.quit()
 
-driver.quit()
+        div_description = soup.find_all('div', {'id': 'product-description'})
+        if div_description:
+            print("looking for product images from description...")
+            img_product_list = div_description[0].find_all('img')
+            for img in img_product_list:
+                print(f"Request for {img['src']}")
+                downloaded = download_image(OUTPUT_DIR, img['src'])
+                if downloaded:
+                    print('Image successfully created.')
+        else:
+            print("div#product-description element not found ")
 
-div_description = soup.find_all('div', {'id': 'product-description'})
-if div_description:
-    print("looking for product images from description...")
-    img_product_list = div_description[0].find_all('img')
-    for img in img_product_list:
-        print(f"Request for {img['src']}")
-        downloaded = download_image(OUTPUT_DIR, img['src'])
-        if downloaded:
-            print('Image successfully created.')
-else:
-    print("div#product-description element not found ")
+if __name__ == '__main__':
+    ali = AliScrapper()
+    ali.scrape()
--- base_scrapper.py
+++ base_scrapper.py
@@ -2,17 +2,28 @@ from abc import ABC, abstractmethod
 import random
 import time
 
+from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 
-class BaseSelemiunScrapper(ABC):
+class BaseSelemiunScrapper:
 
-    def scroll_and_wait(driver, dom_element, delta_x=0, delta_y=0, delay_seconds=None):
+    SCROLL_DELAY_TIMES = [1,2,3,4,5]
+    _driver = None
+
+    def init_driver(self, implicitly_wait=None):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--start-maximized")
+
+        self._driver = webdriver.Chrome(options=options)
+        if implicitly_wait:
+            self._driver.implicitly_wait(implicitly_wait)
+
+    def scroll_and_wait(self, dom_element, delta_x=0, delta_y=0, delay_seconds=None):
         scroll_origin = ScrollOrigin.from_element(dom_element)
-        ActionChains(driver).scroll_from_origin(scroll_origin, delta_x, delta_y).perform()
-        delay_seconds = delay_seconds if delay_seconds else random.choice(SCROLL_DELAY_TIMES)
+        ActionChains(self._driver).scroll_from_origin(scroll_origin, delta_x, delta_y).perform()
+        delay_seconds = delay_seconds if delay_seconds else random.choice(self.SCROLL_DELAY_TIMES)
         time.sleep(delay_seconds)    
 
-    @abstractmethod
     def scrape(self):
         pass
